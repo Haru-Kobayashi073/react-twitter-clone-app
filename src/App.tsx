2f62786e6a2fffe7f68236655d952058b3bf638c
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-01-31 14:46:45 +0900

    reduxにユーザー情報を持たせる

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from './features/userSlice';
+import { auth } from './firebase';
 
-function App() {
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser)=> {
+      if (authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          photoUrl: authUser.photoURL,
+          displayName: authUser.displayName
+        }));
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
   return (
     <div className="App"></div>
   );
