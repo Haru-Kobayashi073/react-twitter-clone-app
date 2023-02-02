c0bacebd189074e2b45a775675d55d79560f5664
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-02-02 11:11:49 +0900

    avatar画像をstorageに格納＆ユーザー情報のredux保持

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from "./Auth.module.css";
 import { useDispatch } from 'react-redux';
+import userSlice, { updateUserProfile } from '../features/userSlice';
 import { auth, provider, storage } from '../firebase';
 
 import { 
@@ -59,16 +60,47 @@ const useStyles = makeStyles((theme) => ({
 
 const Auth:React.FC = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [ email, setEmail ] = useState("");
   const [ password, setPassword ] = useState("");
+  const [username, setUsernamme] = useState("");
+  const [avatarImage, setAvatarImage] = useState<File | null>(null);
   const [ isLogin, setIsLogin ] = useState(true);
+
+  const onChangeImageHandler = (e: any) => {
+    if (e.target.files![0]) {
+      setAvatarImage(e.target.files![0]);
+      e.target.value = '';
+    }
+  }
 
   const signInEmail = async () => {
     await auth.signInWithEmailAndPassword(email, password);
   }
 
   const signUpEmail = async () => {
-    await auth.createUserWithEmailAndPassword(email, password);
+    const authUser = await auth.createUserWithEmailAndPassword(email, password);
+    let url = '';
+    if (avatarImage) {
+      //firebase storageの使用上、同じファイルネームのファイルが2回作られると以前に作られた方のファイルが削除されるのでランダムなファイルネームを生成する
+      const S ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N))).map((n) => S[n % S.length]).join("");
+      const fileName = randomChar + "_" + avatarImage.name; 
+      await storage.ref(`avatars/${fileName}`).put(avatarImage);
+      url = await storage.ref('avatars').child(fileName).getDownloadURL();
+    }
+
+    await authUser.user?.updateProfile({
+      displayName: username,
+      photoURL: url,
+    });
+    dispatch(
+      updateUserProfile({
+        displayName: username,
+        photoUrl: url,
+      })
+    );
   }
 
   const signInGoogle = async () => {
@@ -147,7 +179,7 @@ const Auth:React.FC = () => {
               <Grid item xs>
                 <span className={styles.login_reset}>Forgot password?</span>
               </Grid>
-              <Grid item xs>
+              <Grid item>
                 <span className= {styles.login_toggleMode} onClick={() => setIsLogin(!isLogin)}>
                 {isLogin ? "Create new account ?" : "back to login"}
                 </span>
